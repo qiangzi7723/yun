0a3c1234b9572a07183f1f476ab7ab39ebed7d47
qiangzi7723 <zhiqiang.zeng@linjianqingdeMac-mini.local>
2016-08-22 21:13:15 +0800

    222
    1

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
-var gulp = require('gulp'),               // 載入 gulp
-    gulpSass = require('gulp-sass');    // 載入 gulp-sass
+var gulp = require('gulp'), // 載入 gulp
+    gulpSass = require('gulp-sass'); // 載入 gulp-sass
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     gulp.watch('scss/**/*.scss', ['styles']);
 });
 
-gulp.task('styles', function () {
-    gulp.src('scss/**/*.scss')    // 指定要處理的 Scss 檔案目錄
-        .pipe(gulpSass())         
-        .pipe(gulp.dest('css'));  // 指定編譯後的 css 檔案目錄
+gulp.task('styles', function() {
+    gulp.src('scss/**/*.scss') // 指定要處理的 Scss 檔案目錄
+        .pipe(gulpSass())
+        .pipe(gulp.dest('css')); // 指定編譯後的 css 檔案目錄
+
 });
